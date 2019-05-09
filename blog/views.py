65aa33b3a6988d4cdc597d1dbdf932e9c5c0d26f
kirsie <wojwrobell@gmail.com>
2019-05-09 20:30:54 +0200

    bootstrap and templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Wronka',
+        'title': 'papapapap',
+        'content': 'To samo co w temacie'
+    },
+    {
+        'author': 'Wjtk',
+        'title': 'tez papapapapa',
+        'content': 'hmm, moze?'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>about</h1>')
+    return render(request, 'blog/about.html', {'developer': 'kirsie'})
+
+
+
