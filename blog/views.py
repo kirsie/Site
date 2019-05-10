e9f7fe4fd9acc940ab0b9b1713d53a9d947a33da
kirsie <wojwrobell@gmail.com>
2019-05-10 17:37:21 +0200

    Add data base

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Wronka',
-        'title': 'papapapap',
-        'content': 'To samo co w temacie'
-    },
-    {
-        'author': 'Wjtk',
-        'title': 'tez papapapapa',
-        'content': 'hmm, moze?'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
